daef28f05ae1a92b3528629e4fcea2631388acce
William Herrera <majintassadar@gmail.com>
2015-04-24 00:06:02 -0700

    added fallback caching using a json file

--- framework/cache.class.php
+++ framework/cache.class.php
@@ -1,5 +1,6 @@
 <?php
 /**
+ * CorePHP
  * @author wherrera
  */
 class Cache {
@@ -7,6 +8,7 @@ class Cache {
     private static $init        = FALSE;
     private static $apc_enabled = FALSE;
     private static $map         = array();
+    private static $cacheFile   = 'cache/cache.json';
     
     private static function init() {
         if(self::$init) {
@@ -16,9 +18,36 @@ class Cache {
         self::$apc_enabled =    function_exists('apc_exists') &&
                                 function_exists('apc_store') &&
                                 function_exists('apc_fetch');
+        if( self::$apc_enabled == FALSE ) {
+            self::loadCacheFile();
+        }
+    }
+    
+    private static function saveCacheFile () {
+        return file_put_contents(self::$cacheFile, json_encode(self::$map));
+    }
+    
+    private static function loadCacheFile () {
+        if( file_exists(self::$cacheFile) ) {
+            $file = file_get_contents(self::$cacheFile);
+            if($file != FALSE) {
+                $map = json_decode($file, true);
+                if($map != NULL) {
+                    self::$map = array_merge(self::$map, $map);
+                    return TRUE;
+                }
+            }
+        }
+        return FALSE;
+    }
+    
+    public static function setCacheFile ($path) {
+        self::$cacheFile = $path;
+        self::loadCacheFile();
     }
     
     /**
+     * Checks if the specified key exist in the data store
      * @param string $key
      * @return mixed <b>TRUE</b> if the key exists, otherwise <b>FALSE</b>
      */
@@ -30,15 +59,31 @@ class Cache {
         return apc_exists($key);
     }
 
+    /**
+     * Cache a variable in the data store
+     * @param string $key
+     * Store the variable using this name.keys are
+     * cache-unique, so storing a second value with the same
+     * key will overwrite the original value.
+     * @param string $value
+     * @param int $ttl (optional) time to live in seconds
+     * @return bool <b>TRUE</b> on success or <b>FALSE</b> on failure.
+     */
     public static function store ($key, $value, $ttl = 0) {
         self::init();
         if(self::$apc_enabled == FALSE) {
            self::$map[$key] = $value;
+           self::saveCacheFile();
            return TRUE;
         }
         return apc_store($key, $value, $ttl);
     }
     
+    /**
+     * Fetch a variable from data store
+     * @param string $key
+     * @return mixed The stored variable or array of variables on success; <b>FALSE</b> on failure
+     */
     public static function fetch ($key) {
         self::init();
         if(self::$apc_enabled == FALSE) {
@@ -47,11 +92,17 @@ class Cache {
         return apc_fetch($key);
     }
     
+    /**
+     * Deletes a variable from the data store
+     * @param string $key
+     * @return mixed <b>TRUE</b> on success or <b>FALSE</b> on failure.
+     */
     public static function delete ($key) {
         self::init();
         if(self::$apc_enabled == FALSE) {
             if( isset(self::$map[$key]) ) {
-                unset( self::$map[$key] );
+                unset(self::$map[$key]);
+                self::saveCacheFile();
             }
             return TRUE;
         }
